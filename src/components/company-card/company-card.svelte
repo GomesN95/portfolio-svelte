40cfa4deb690f64adf67070b49a161a9f512706c
Nicolas Gomes <gomes.nicolas95@gmail.com>
2023-10-06 01:20:51 +0200

    [WIP] company card

---
+++ src/components/company-card/company-card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let name: string = '';
+	export let description: string = '';
+	import './company-card.scss';
+</script>
+
+<div class="company-card">
+	<div class="content">
+		<div class="name">{name}</div>
+		<div class="description">{description}</div>
+		<div class="tags">
+			<div class="tag">Node.JS</div>
+			<div class="tag">React.JS</div>
+		</div>
+	</div>
+</div>
