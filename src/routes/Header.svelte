b83045176c4b1e3e960f25ff373cb88f57b4022c
Nicolas Gomes <gomes.nicolas95@gmail.com>
2023-10-07 01:59:01 +0200

    [WIP] Page contact

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,8 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={$page.url.pathname === '/contact' ? 'page' : undefined}>
+				<a href="/contact">Contact</a>
 			</li>
 		</ul>
 	</nav>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>Contact</title>
+	<meta name="description" content="Contact Nicolas Gomes" />
+</svelte:head>
+
+<script>
+	import './page.scss';
+</script>
+
+<div id="contact">
+	<h1>Contact</h1>
+
+	<div class="content">
+		<form class="form" action="">
+			<label class="email">
+				Email
+				<input name="email" type="email" placeholder="obiwan.kenobi@galactic-senate.jedi"/>
+			</label>
+			<label class="first-name">
+				First Name
+				<input name="text" type="text" placeholder="Obi-Wan" />
+			</label>
+			<label class="last-name">
+				Last Name
+				<input name="text" type="First Name" placeholder="Kenobi" />
+			</label>
+			<label class="message">
+				Message
+				<textarea name="message" rows="5" cols="33"  placeholder="Hello there ..."/>
+			</label>
+			<button>Send</button>
+		</form>
+	</div>
+</div>
